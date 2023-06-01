2e2d6205416aead0a1f0c22e852c60410a413db5
Mickalu <lucasmichaeli95@gmail.com>
2023-06-01 19:52:35 +0200

    [master] - front return App container

---
+++ Frontend/frontend_app/src/container/AppContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import App from '../component/App';
+
+const AppContainer = () => (
+  <App/>
+);
+
+export default AppContainer;
---
+++ Frontend/frontend_app/src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+import AppContainer from './container/AppContainer';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <AppContainer/>
+);
+
+reportWebVitals();
